e97a7fd3f2c4c27cbc2dd580b6beb35cf947c402
Kevin Pham <kvnvpham@gmail.com>
2022-10-31 13:34:31 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from datetime import date
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    with open("data.json") as file:
+        data = json.load(file)
+
+    return render_template("index.html", projects=data)
+
+
+@app.context_processor
+def inject_copyright():
+    return {"year": date.today().year}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
